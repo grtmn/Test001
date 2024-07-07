8edc6b10d7605ca39546e43db5c9b48298ac6db8
grtmn0224 <grtmn0224@gmail.com>
2024-07-08 01:18:48 +0900

    c9

--- gitTest/Form1.Designer.cs
+++ gitTest/Form1.Designer.cs
@@ -39,6 +39,7 @@
             this.label1.Size = new System.Drawing.Size(52, 18);
             this.label1.TabIndex = 0;
             this.label1.Text = "ok git";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // Form1
             // 
@@ -48,6 +49,7 @@
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
